181dc8bb5681dfc644396e0a0725a1e5c719338c
Elisa Warner <elisawa@umich.edu>
2020-02-28 13:32:05 -0500

    First commit

---
+++ hello_world.py
@@ -0,0 +1,8 @@
+####################################
+## Hello World
+## Author: Elisa Warner
+## Date: 02/28/2020
+## Description: A simple Hello, World Program
+####################################
+
+print('Hello World')
